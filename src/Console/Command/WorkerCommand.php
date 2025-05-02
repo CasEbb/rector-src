f6883f03f16a0937efa0e7e4508940f0119cbe94
Tomas Votruba <tomas.vot@gmail.com>
2025-05-02 22:04:31 +0200

    use isNames() from parnet rector

--- src/Console/Command/WorkerCommand.php
+++ src/Console/Command/WorkerCommand.php
@@ -109,6 +109,7 @@ final class WorkerCommand extends Command
         } else {
             $preFileCallback = null;
         }
+
         // 1. handle system error
         $handleErrorCallback = static function (Throwable $throwable) use ($encoder): void {
             $systemError = new SystemError($throwable->getMessage(), $throwable->getFile(), $throwable->getLine());
